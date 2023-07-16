7bd58b339d8ab87467fa773594390a1bf5e0c16c
hamid reza <reza.attar1375@gmail.com>
2023-07-16 15:12:21 +0330

    first

---
+++ lib2/module/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const App = () => {
+  return /*#__PURE__*/React.createElement("div", null, "App");
+};
+export default App;
+//# sourceMappingURL=App.js.map
---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View, Text} from 'react-native-web'
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize:20}} >salam</Text>
+      <Text style={{fontSize:20}} >salam</Text>
+      <Text style={{fontSize:20}} >salam</Text>
+      <Text style={{fontSize:20}} >salam</Text>
+      <Text style={{fontSize:17}} >salam</Text>
+      <Text style={{fontSize:17}} >salam</Text>
+      <Text style={{fontSize:17}} >salam</Text>
+      <Text style={{fontSize:17}} >salam</Text>
+      <Text >salam</Text>
+      <Text >salam</Text>
+      <Text >salam</Text>
+      <Text >salam</Text>
+    </View>
+  )
+}
+
+export default App

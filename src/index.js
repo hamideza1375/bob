88aeebbf815e2d879381fa7ba4544e1d951c1365
hamid reza <reza.attar1375@gmail.com>
2023-07-16 19:58:40 +0330

    ssr

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from "react-dom/client";
+import ReactDOMServer from "react-dom/server";
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
+const html = ReactDOMServer.renderToString(<App/>);
+
+console.log(html);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <App />
+);
+reportWebVitals();
 
 
 
